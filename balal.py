f159f8725a6f6ee37888d2747d3014090b122cb2
emminka <emma.krompascikova@gmail.com>
2022-10-24 01:21:03 +0200

    upgraded button

--- balal.py
+++ balal.py
@@ -2,10 +2,14 @@ import tkinter as tk
 #import tkinter.ttk as ttk
 from tkVideoPlayer import TkinterVideo
 
+
 purple_clickable_button= "#DCD0FF"
+purple_clickable_button_clicked= "#C6B5FF"
 grey_not_clickable_button= "#B2B0B9"
 app_bg = "#6D6A75"
 button_text="#3A3541"
+
+font_buttons = ("Leelawadee UI Semilight", 17, "bold")
 
 window = tk.Tk()
 window.title("Subtitle remover")
@@ -20,15 +24,15 @@ videoplayer.pack(expand=True, fill="both")
 videoplayer.play() # play the video
 
 bottomframe = tk.Frame(window)
-bottomframe.pack( side = tk.BOTTOM )
+bottomframe.pack( side = tk.BOTTOM, pady = 12)
 
-b_upload = tk.Button(bottomframe, text="Upload Video",bg=purple_clickable_button, fg=button_text,width=20, height=4).pack(side=tk.LEFT)
+b_upload = tk.Button(bottomframe, relief="flat" ,text="Upload Video",activebackground =purple_clickable_button_clicked,font = font_buttons, bg=purple_clickable_button, fg=button_text,width=17, height=2, state="normal").pack(side=tk.LEFT,padx=30)
 #b_upload.place(anchor=tk.CENTER,x=209, y=550)
 
-b_selectarea = tk.Button(bottomframe, text="Select Area",bg=grey_not_clickable_button,fg=button_text,width=20, height=4).pack(side=tk.LEFT)
+b_selectarea = tk.Button(bottomframe,relief="flat", text="Select Area",font = font_buttons, bg=grey_not_clickable_button,fg=button_text,width=17, height=2, state="disabled").pack(side=tk.LEFT,padx=30)
 #b_selectarea.place(anchor=tk.CENTER,x=509, y=550)
 
-b_deleteSub = tk.Button(bottomframe, text="Delete Subtitles",bg=grey_not_clickable_button,fg=button_text,width=20, height=4).pack(side=tk.LEFT)
+b_deleteSub = tk.Button(bottomframe, relief="flat",text="Delete Subtitles", font = font_buttons,bg=grey_not_clickable_button,fg=button_text,width=17, height=2, state="disabled").pack(side=tk.LEFT,padx=30)
 #b_deleteSub.place(anchor=tk.CENTER,x=809, y=550)
 
 window.mainloop()
