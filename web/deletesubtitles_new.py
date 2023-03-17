04cb2eb0cddb5d5609942b718b27aa15ad3c6466
emminka <emma.krompascikova@gmail.com>
2023-03-17 23:30:09 +0100

    added select to web

--- web/deletesubtitles_new.py
+++ web/deletesubtitles_new.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import numpy as np
 import random
 from random import randrange
@@ -19,7 +20,7 @@ widthOfVideo = None
 argv = sys.argv[1:]
 
 try:
-    opts, args = getopt.getopt(argv, "a:b:c:d:e:f:g:")
+    opts, args = getopt.getopt(argv, "a:b:c:d:e:f:g:h:")
     
 except:
     print("Error")
@@ -27,27 +28,30 @@ except:
 for opt, arg in opts:
     if opt in ['-a']:
         xL = int(arg)
-        print("zapisujem lave x", xL);
+        print("zapisujem lave x", xL)
     elif opt in ['-b']:
         yL = int(arg)
-        print("zapisujem lave y",yL);
+        print("zapisujem lave y",yL)
     elif opt in ['-c']:
         xR = int(arg)
-        print("zapisujem prave x",xR);
+        print("zapisujem prave x",xR)
     elif opt in ['-d']:
         yR = int(arg)  
-        print("zapisujem prave y", yR);
+        print("zapisujem prave y", yR)
     elif opt in ['-e']:
         filepath = arg  
-        print("cesticka", filepath);
+        print("cesticka", filepath)
     elif opt in ['-f']:
         heightOfVideo = int(arg)
-        print("heightOfVideo", heightOfVideo);
+        print("heightOfVideo", heightOfVideo)
     elif opt in ['-g']:
         widthOfVideo = int(arg)  
-        print("widthOfVideo", widthOfVideo);
+        print("widthOfVideo", widthOfVideo)
+    elif opt in ['-h']:
+        methodOfRemoving = int(arg)  
+        print("sposob odstranenia", methodOfRemoving); #0 default, 1 keras
          
-if(xL is None or yL is None or xR is None or yR is None or filepath is None):
+if(xL is None or yL is None or xR is None or yR is None or filepath is None or heightOfVideo is None or  widthOfVideo is None or methodOfRemoving is None):
     print("dovidopo exitujeeme ,daco  je plano")
     sys.exit(1)
 
@@ -55,9 +59,10 @@ if(xL is None or yL is None or xR is None or yR is None or filepath is None):
 video = cv2.VideoCapture(filepath)
 fpska = video.get(cv2.CAP_PROP_FPS)
 
-koncovka = "_no_subtitles.mp4"
+koncovka = "_noSUB_noSOUND.mp4"
 new_name_same_path = filepath.rsplit(".", 1)[0];
 new_name_same_path += koncovka;
+print(new_name_same_path)
 
 koncovka1 = "_noSUB_yesSOUND.mp4"
 new_name_same_path1 = filepath.rsplit(".", 1)[0];
@@ -98,6 +103,7 @@ audio_clip = AudioFileClip(filepath)   #audio ziskavam lebo cv2 nepouziva
 modified_clip = VideoFileClip(new_name_same_path)
 final_clip = modified_clip.set_audio(audio_clip)
 final_clip.write_videofile(new_name_same_path1)
+os.remove(new_name_same_path)
 
 
 print("Video has been released.")
--- web/kerass.py
+++ web/kerass.py
@@ -2,27 +2,62 @@ import matplotlib.pyplot as plt
 
 import keras_ocr
 import os
+import sys
 
 # keras-ocr will automatically download pretrained
 # weights for the detector and recognizer.
+sourceFile  = open('output.txt','w')
 pipeline = keras_ocr.pipeline.Pipeline()
 
+pathto1 = r'C:\Users\Emma\Desktop\Bakalarka\videos\videa_bakalarka\text1.PNG'
+pathto11 = r'C:\Users\Emma\Desktop\Bakalarka\videos\videa_bakalarka\text1.PNG'
+pathto111 = r'C:\Users\Emma\Desktop\Bakalarka\videos\videa_bakalarka\text1.PNG'
+pathto1111 = r'C:\Users\Emma\Desktop\Bakalarka\videos\videa_bakalarka\text1.PNG'
+
+pathto2 = r'C:\Users\Emma\Desktop\Bakalarka\videos\videa_bakalarka\text2.PNG'
+pathto22 = r'C:\Users\Emma\Desktop\Bakalarka\videos\videa_bakalarka\text2.PNG'
+pathto3 = r'C:\Users\Emma\Desktop\Bakalarka\videos\videa_bakalarka\no_text.PNG'
+
+
+cesticky=[pathto1]
+cesticky2=[pathto2, pathto22,pathto3]
+cesticky3=[pathto3, pathto1]
+print(cesticky)
 # Get a set of three example images
 images = [
-    keras_ocr.tools.read(url) for url in [
-        'https://upload.wikimedia.org/wikipedia/commons/b/b4/EUBanana-500x112.jpg',
-        'https://scontent-prg1-1.cdninstagram.com/v/t51.2885-15/331661481_5794284590685613_2433262251339321851_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent-prg1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=wYtFtjxtHJEAX9W2rHi&edm=AAAAAAABAAAA&ccb=7-5&ig_cache_key=MzA0MTY2NDEwODM1Nzg5ODI0NA%3D%3D.2-ccb7-5&oh=00_AfC9OZMtL1LktCKQ3-eJqwRnWQbn7zKp6s_j1LhGPf8MRA&oe=63F6B161&_nc_sid=022a36'
-    ]
+    keras_ocr.tools.read(img) for img in cesticky
 ]
 
+images2 = [
+    keras_ocr.tools.read(img) for img in cesticky2
+]
+
+images3 = [
+    keras_ocr.tools.read(img) for img in cesticky3
+]
+
+print(len(images))
+print(len(images2))
 # Each list of predictions in prediction_groups is a list of
 # (word, box) tuples.
 prediction_groups = pipeline.recognize(images)
+#prediction_groups2 = pipeline.recognize(images2)
+#prediction_groups3 = pipeline.recognize(images3)
+
 
 # Plot the predictions
-fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
-for ax, image, predictions in zip(axs, images, prediction_groups):
-    keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
+#fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
+#for ax, image, predictions in zip(axs, images, prediction_groups):
+   # keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
 
-plt.show()
+print("huhuh")
+#print(prediction_groups, file = sourceFile)
+#print(prediction_groups)
+
+for x in range(len(images)):
+    print("number",x)
+    for text, box in prediction_groups[x]:
+        print(text)
+
+#plt.show()
 
